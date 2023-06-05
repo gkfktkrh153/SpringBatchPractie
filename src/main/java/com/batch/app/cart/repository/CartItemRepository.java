bd3e15bc1dfc62f8468a3c055c29861daa987125
gkfktkrh153 <gkfktkrh153@naver.com>
2023-06-05 17:08:00 +0900

    도매가 추가, 노출용 색상 사이즈 추가

--- src/main/java/com/batch/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/batch/app/cart/repository/CartItemRepository.java
@@ -4,5 +4,10 @@ package com.batch.app.cart.repository;
 import com.batch.app.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByMemberIdAndProductOptionId(Long memberId, Long productOptionId);
+    List<CartItem> findAllByMemberId(Long memberId);
 }
